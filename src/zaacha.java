6e88e3d3d61b6202bf90eb2804158587a899b2f1
sergei-paht <77566158+sergei-paht@users.noreply.github.com>
2021-05-10 15:34:09 +0300

    Add files via upload

---
+++ src/zaacha.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class zaacha {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your ip address");
+        String str = in.nextLine();// вводим строчку
+        boolean answer = str.matches("([0-9]\\.|[0-9][0-9]\\.|1[0-9][0-9]\\.|2[0-4][0-9]\\.|25[0-5]\\.){3}([0-9]|[0-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+        if(answer){
+            System.out.println("The address is correct");
+        }
+        else{
+            System.out.println("Invalid address");
+        }
+    }
+}
